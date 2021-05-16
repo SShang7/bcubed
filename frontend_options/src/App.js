aac876113890128b2d28a2fb4e91dae6eb4c63c0
hoodjustin <justinnerhood@yahoo.com>
2021-05-15 17:44:06 -0700

    Big layout update
    basically redid the website layout with a much nicer looking navigation bar as well as much more flexibility in the code

--- frontend_options/src/App.js
+++ frontend_options/src/App.js
@@ -1,92 +1,23 @@
-/*import React from 'react';
+//<p> My token = {window.token}</p>
+
+import Navbar from './components/navbar';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
-import { Layout,Avatar,Menu,Breadcrumb } from 'antd';
-import { UserOutlined,AppstoreOutlined, SettingOutlined  } from '@ant-design/icons';
-import { Typography } from 'antd';
-const { Title } = Typography;
-const { SubMenu } = Menu;
-
-const { Header, Footer, Sider, Content } = Layout;
-
-
+//import Home from './components/pages/Home'
+//import Trends from './components/pages/Trends';
+//import OPC from './components/pages/OPC';
+//import AboutUs from './components/pages/AboutUs';
 
 function App() {
   return (
-    <div className="App">
-      <Layout>
-      <Header style={{padding:10}}>
-        <Avatar style={{float:"right"}} size="large" icon={<UserOutlined />} />
-        <Title style={{color:'white'}} level={3}>bullish.bearish.beyond</Title>
-      </Header>
-        <Layout>
-        <Sider>
-          <Menu
-          defaultSelectedKeys={["Menu"]}
-          mode="inline"
-          >
-            <Menu.Item type="Menu">
-              Menu
-            </Menu.Item>
-            <SubMenu>    
-              <Menu.ItemGroup key="about us" title="about us">
-                <Menu.Item key="location">
-                  Our Location
-                </Menu.Item>
-              </Menu.ItemGroup>
-            </SubMenu>
-          </Menu>
-        </Sider>
-        <Layout>
-        <Content style={{ padding: '0 50px' }}>
-      <Breadcrumb style={{ margin: '16px 0' }}>
-      <Breadcrumb.Item>Home</Breadcrumb.Item>
-        <Breadcrumb.Item>
-        <p> My token = {window.token}</p>
-        </Breadcrumb.Item>
-      </Breadcrumb>
-      <div className="site-layout-content" style={{ background: '#fff', padding: 24, minHeight: 580}}>Content</div>
-    </Content>
-    <Footer style={{ textAlign: 'center' }}>bullish.bearish.beyond llc</Footer>
-        </Layout>
-      </Layout>
-    </Layout>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
-export default App;*/
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
-import './index.css';
-import { Layout, Menu, Breadcrumb, Avatar, Typography } from 'antd';
-import { UserOutlined,AppstoreOutlined, SettingOutlined  } from '@ant-design/icons';
-const { Title } = Typography
-const { Header, Content, Footer } = Layout;
-
-function App() {
-  return(
-    <div className="App">
-    <Layout className="layout">
-      <Header>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">data</Menu.Item>
-          <Menu.Item key="2">options profit calculator</Menu.Item>
-          <Menu.Item key="3">about us</Menu.Item>
-        </Menu>
-      </Header>
-      <Content style={{ padding: '0 50px' }}>
-        <Breadcrumb style={{ margin: '16px 0' }}>
-          <Breadcrumb.Item>Home</Breadcrumb.Item>
-        </Breadcrumb>
-        <div className="site-layout-content">
-        <p> My token = {window.token}</p>
-        </div>
-      </Content>
-      <Footer style={{ textAlign: 'center' }}>bullish.bearish.beyond.llc</Footer>
-    </Layout>
-    </div>
-  );
-}
 export default App;
---
+++ frontend_options/src/components/navbar.js
@@ -0,0 +1,73 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from "react-router-dom"
+import './navbar.css';
+import { Button } from './Button';
+import { AiOutlineStock } from "react-icons/ai"
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true);
+        }
+    };
+
+    useEffect(() => {
+        showButton()
+    }, [])
+
+    window.addEventListener('resize', showButton);
+
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+                        bcubed
+                        <AiOutlineStock />
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/trends' className='nav-links' onClick={closeMobileMenu}>
+                                trends
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/opc' className='nav-links' onClick={closeMobileMenu}>
+                                options profit calc
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/aboutus' className='nav-links' onClick={closeMobileMenu}>
+                                about us
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                                sign up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>sign up</Button>}
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar;
