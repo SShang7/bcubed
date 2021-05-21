696f991ada722e25660d676e7886c3d332ae6126
hoodjustin <justinnerhood@yahoo.com>
2021-05-21 13:33:07 -0700

    Updated login page + created Sign in page
    Updated the layout of the login page plus added forms so that input can be stored and sent to backend via POST. Also created the sign up page and added forms to the sign up page as well

--- frontend_options/src/components/pages/SignUp.js
+++ frontend_options/src/components/pages/SignUp.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./SignUp.css";
 
-function SignUp() {
-    return (
-        <div>
-            <h1 className="title is-1">This is the signup Page</h1>
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  function validateForm() {
+    return username.length > 0 && password.length > 0 && email.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="Login" align="center">
+      <Form onSubmit={handleSubmit}>
+        <div classname="Email">
+            <Form.Group size="lg" controlId="email">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+                autoFocus
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} //e.target.value holds email
+            />
+            </Form.Group>
         </div>
-    )
+        <div className="UserName">
+            <Form.Group size="lg" controlId="username">
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+                autoFocus
+                type="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)} //e.target.value holds username
+            />
+        </Form.Group>
+        </div>
+        <div className="PW">
+            <Form.Group size="lg" controlId="password">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)} //e.target.value holds password
+            />
+            </Form.Group>
+        </div>
+        <div className="loginbutton">
+            <Button block size="lg" type="submit" disabled={!validateForm()}>
+            Create Account
+            </Button>
+        </div>
+      </Form>
+    </div>
+  );
 }
 
-export default SignUp
