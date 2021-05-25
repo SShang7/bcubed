053075c79eeaeb622e524fe2044a942e61773235
1uka5 <lukasbroc@gmail.com>
2021-05-24 19:03:11 -0700

    trends rough
    displays text and takes text input

--- frontend_options/src/components/pages/Trends.js
+++ frontend_options/src/components/pages/Trends.js
@@ -1,12 +1,41 @@
-import React from 'react'
-import '../../App.css'
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./Trends.css";
+import { Link } from "react-router-dom"
 
+export default function Trends() {
+  const [ticker, setTicker] = useState("");
 
-const Trends = () => (
-    <div>
-      <h1 className="title is-1">This is the trends Page</h1>
-      <p> My token = {window.token}</p>
+  function validateForm() {
+    return ticker.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div className="TrendSubmit" align="center">
+      <Form onSubmit={handleSubmit}>
+        <div className="trendintro">
+          <h1>This is the trends page!</h1>
+          <h2>Select a ticker:</h2>
+        </div>
+        <div className="Ticker">
+          <Form.Group size="lg" controlId="ticker">
+            <Form.Control
+              autoFocus
+              type="ticker"
+              value={ticker}
+              onChange={(e) => setTicker(e.target.value)} //e.target.value holds username
+            />
+          </Form.Group>
+          <Button block size="sm" type="submit" disabled={!validateForm()}>
+            Go
+          </Button>
+        </div>
+      </Form>
     </div>
   );
-  
-  export default Trends;
+}
