9e53da63aa464608ac4966256b219600e67e8291
1uka5 <lukasbroc@gmail.com>
2021-05-25 19:48:38 -0700

    Trends skeleton
    Trends can now take input and is set up to fetch data based on that input. Loading screen, display for when .png is successfully retrieved, display for when .png is not successfully retrieved (which is what you'll see regardless of input text for now).

--- frontend_options/src/components/pages/Trends.js
+++ frontend_options/src/components/pages/Trends.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./Trends.css";
-import { Link } from "react-router-dom"
 
 export default function Trends() {
   const [ticker, setTicker] = useState("");
+  const [button, setButton] = useState(false);
+  const [success, setSuccess] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [data, setData] = useState(null);
 
   function validateForm() {
     return ticker.length > 0;
@@ -15,27 +18,71 @@ export default function Trends() {
     event.preventDefault();
   }
 
+  function clicked() {
+    const myRequest = new Request({ ticker }, { //fetched url TBD
+      /*
+      method: 'GET',
+      headers: myHeaders,
+      mode: 'cors',
+      cache: 'default',
+      */
+      //pretty sure this isn't necessary
+    });
+    setButton(true);
+    fetch(myRequest)
+      .then((response) => {
+        setLoading(false);
+        if (response.ok) {
+          return response.json();
+        }
+        setSuccess(false);
+      })
+      .then((data) => {
+        setData(data);
+      })
+      .catch((error) => {
+        console.log("Something went wrong.", error);
+      });
+  };
+  
   return (
-    <div className="TrendSubmit" align="center">
-      <Form onSubmit={handleSubmit}>
-        <div className="trendintro">
-          <h1>This is the trends page!</h1>
-          <h2>Select a ticker:</h2>
-        </div>
-        <div className="Ticker">
-          <Form.Group size="lg" controlId="ticker">
-            <Form.Control
-              autoFocus
-              type="ticker"
-              value={ticker}
-              onChange={(e) => setTicker(e.target.value)} //e.target.value holds username
-            />
-          </Form.Group>
-          <Button block size="sm" type="submit" disabled={!validateForm()}>
-            Go
+    button ?
+      (loading ? (<div className="loading">
+        <h1>Loading...</h1>
+      </div>) :
+        (success ?
+          (<div>
+            <div className="trendinfo">
+              <h1>Trends for {ticker}:</h1>
+            </div>
+            <div className="image">
+              <img src="" alt="test"/>
+            </div>
+          </div>) :
+          (<div className="failure">
+            <h1>Could not fetch data on trends for {ticker}:</h1>
+          </div>)))
+      :
+      (<div className="TrendSubmit" align="center">
+        <Form onSubmit={handleSubmit}>
+          <div className="trendintro">
+            <h1>This is the trends page!</h1>
+            <h2>Select a ticker:</h2>
+          </div>
+          <div className="Ticker">
+            <Form.Group size="lg" controlId="ticker">
+              <Form.Control
+                autoFocus
+                type="ticker"
+                value={ticker}
+                onChange={(e) => setTicker(e.target.value)}
+              />
+            </Form.Group>
+            <Button block onClick={() => clicked()} size="sm" type="submit" disabled={!validateForm()}>
+              Go
           </Button>
-        </div>
-      </Form>
-    </div>
+          </div>
+        </Form>
+      </div>)
   );
 }
