6a6ce773938154f3f7dd03e40c1028641c96b5aa
zikun13 <82916366+zikun13@users.noreply.github.com>
2021-05-29 11:38:00 -0700

    Update App.js
    Verify the application was initialized correctly

--- frontend_options/src/App.js
+++ frontend_options/src/App.js
@@ -10,6 +10,21 @@ import AboutUs from './components/pages/AboutUs';
 import Login from './components/pages/Login';
 import SignUp from './components/pages/SignUp';
 import Watchlist from './components/pages/Watchlist';
+import React from "react";
+import firebase from "firebase";
+
+export default function App() {
+  const firebaseApp = firebase.apps[0];
+  return (
+    <div>
+      <h1>React & Firebase</h1>
+      <h2>By @farazamiruddin</h2>
+      <code>
+        <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
+      </code>
+    </div>
+  );
+}
 
 function App() {
   return (
