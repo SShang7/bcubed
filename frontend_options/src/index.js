3e0b0f45e7cac58c22c7ffdf61e77288c1f7e7a6
zikun13 <82916366+zikun13@users.noreply.github.com>
2021-05-29 11:30:57 -0700

    add firebase
    initialize firebase

--- frontend_options/src/index.js
+++ frontend_options/src/index.js
@@ -3,6 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from "firebase/app";
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBlNDrtqbuv8klfgN8R_jan_UyEdsoISLY",
+  authDomain: "b3b3-66fec.firebaseapp.com",
+  databaseURL: "https://b3b3-66fec-default-rtdb.firebaseio.com",
+  projectId: "b3b3-66fec",
+  storageBucket: "b3b3-66fec.appspot.com",
+  messagingSenderId: "503843612140",
+  appId: "1:503843612140:web:a3dc05b103b7e85b440cc9",
+  measurementId: "G-XXLL1QB318"
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
