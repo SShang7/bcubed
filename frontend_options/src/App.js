db4aa496254abe47456f2770e102cc10a5e552ee
Sammy Shang <sammy7shang@gmail.com>
2021-05-29 12:44:39 -0700

    Revert "revert firebase changes"
    This reverts commit 83a4f3c84397bcd5d5bcb0e8506ac02a204934ec.

--- frontend_options/src/App.js
+++ frontend_options/src/App.js
@@ -9,7 +9,6 @@ import OPC from './components/pages/OPC';
 import AboutUs from './components/pages/AboutUs';
 import Login from './components/pages/Login';
 import SignUp from './components/pages/SignUp';
-<<<<<<< HEAD
 import Watchlist from './components/pages/Watchlist';
 import React from "react";
 import firebase from "firebase/app";
@@ -84,8 +83,6 @@ export const SignupForm = ({onSuccess}) => {
    );
  };
 
-=======
->>>>>>> parent of 251f546 (added watchlist tab)
 
 function App() {
   return (
@@ -99,6 +96,7 @@ function App() {
           <Route path='/aboutus' component={AboutUs} />
           <Route path='/login' component={Login} />
           <Route path='/signup' component={SignUp} />
+          <Route path='/watchlist' component={Watchlist} />
         </Switch>
       </Router>
     </>
