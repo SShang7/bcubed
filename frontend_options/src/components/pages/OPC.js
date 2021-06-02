ecb0b00226201ca9eb1327ded72c7616970f9a7e
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2021-06-01 22:01:58 -0700

    Added OPC.css Updated OPC.js
    Added option to choose whether you want to use the long call or long put calculator
    
    Made the "Buy or Write" option save onto the state of the Form.

--- frontend_options/src/components/pages/OPC.js
+++ frontend_options/src/components/pages/OPC.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import './OPC.css';
 
 class Form extends React.Component {
 	constructor(props) {
@@ -7,7 +8,9 @@ class Form extends React.Component {
 					  contracts: '',
 					  ppo: '',
 					  symbol: '',
-					  stockPrice: ''
+					  stockPrice: '',
+					  buyOrWrite: '',
+					  callOrPut: ''
 		};
 
 		this.handlePPOChange = this.handlePPOChange.bind(this);
@@ -16,6 +19,8 @@ class Form extends React.Component {
 		this.handleSymbolChange = this.handleSymbolChange.bind(this);
 		this.handleStockPriceChange = this.handleStockPriceChange.bind(this);
 		this.handleLookUp = this.handleLookUp.bind(this);
+		this.handleCallOrPutChange = this.handleCallOrPutChange.bind(this);
+		this.handleBuyOrWriteChange = this.handleBuyOrWriteChange.bind(this);
 	}
 	
 	getTotal(contracts, ppo) {
@@ -51,10 +56,20 @@ class Form extends React.Component {
 	handleStockPriceChange(event) {
 		this.setState({stockPrice: event.target.value});
 	}
+	
+	handleCallOrPutChange(event) {
+		console.log('Changed callOrPut to ' + event.target.value);
+		this.setState({callOrPut: event.target.value});
+	}
+
+	handleBuyOrWriteChange(event) {
+		console.log('Changed buyOrWrite to ' + event.target.value);
+		this.setState({buyOrWrite: event.target.value});
+	}
 
 	handleSubmit(event) {
 		alert('Submitted stock ' + this.state.symbol + ' with stock price $' + this.state.stockPrice +
-			'. You chose to buy ' + (parseInt(this.state.contracts) * 100) + ' contracts at $' + this.state.ppo + ' per option.' );
+			'. You chose to ' + this.state.buyOrWrite + ' ' + (parseInt(this.state.contracts) * 100) + ' contracts at $' + this.state.ppo + ' per option.' );
 		event.preventDefault();
 	}
 	
@@ -67,14 +82,32 @@ class Form extends React.Component {
 		return (
 			<form>
 				<div>
+					<label className="label">
+						<h3 className="h3">
+							What kind of calculator did you want?
+						</h3>
+						
+						<br/>
+						
+						<input type="radio" id="long call" name="Call or Put" value="long call" checked={this.state.callOrPut === "long call"} onChange={this.handleCallOrPutChange} />
+						<label for="long call"> Long Call </label>
+						
+						<input type="radio" id="long put" name="Call or Put" value="long put" checked={this.state.callOrPut === "long put"} onChange={this.handleCallOrPutChange} />
+						<label for="long put"> Long Put </label>
+					</label>
+				</div>
+				
+				<br/>
+			
+				<div>
 					<div>
-						<h3> Stock Symbol </h3>
+						<h3 className="h3"> Stock Symbol </h3>
 					</div>
 					
 					<br/>
 					
 					<span>
-						<label>
+						<label className="label">
 							Symbol:
 							<input type="text" name="Symbol" onChange={this.handleSymbolChange}/>
 						</label>
@@ -83,7 +116,7 @@ class Form extends React.Component {
 					
 					<br/>
 					
-					<label>
+					<label className="label">
 						Stock Price:
 						<span>
 							$
@@ -92,25 +125,27 @@ class Form extends React.Component {
 					</label>
 				</div>
 				
+				<br/>
+				
 				<div>
 					<div>
-						<h3> Option </h3> 
+						<h3 className="h3"> Option </h3> 
 					</div>
 					
 					<br/>
 					
-					<label>
+					<label className="label">
 						Buy or write:
-						<input type="radio" id="buy" name="Buy or Write" value="buy" />
+						<input type="radio" id="buy" name="Buy or Write" value="buy" checked={this.state.buyOrWrite === "buy"} onChange={this.handleBuyOrWriteChange} />
 						<label for="buy"> Buy </label>
 						
-						<input type="radio" id="write" name="Buy or Write" value="buy" />
+						<input type="radio" id="write" name="Buy or Write" value="write" checked={this.state.buyOrWrite === "write"} onChange={this.handleBuyOrWriteChange} />
 						<label for="write"> Write </label>
 					</label> 
 					
 					<br/>
 					
-					<label>
+					<label className="label">
 						Price per Option:
 						<span>
 							$
@@ -120,7 +155,7 @@ class Form extends React.Component {
 					
 					<br/>
 					
-					<label>
+					<label className="label">
 						Contracts:
 						<span>
 							<input type="text" name="Contracts" id="Contracts" onChange={this.handleContractChange} />
@@ -130,7 +165,7 @@ class Form extends React.Component {
 					
 					<br/>
 					
-					<label>
+					<label className="label">
 						Total Cost:
 						<div id="total" value={this.state.totalCost}>{this.state.totalCost}</div>
 					</label>
