049bed6243086995423c256dc84921c94114ecaa
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2021-06-01 18:47:40 -0700

    Update OPC.js
    Fixed calls to handleLookUp and handleStockPriceChange to correctly update the state variables.
    
    Fixed total cost calculation being one step behind the actual calculation.

--- frontend_options/src/components/pages/OPC.js
+++ frontend_options/src/components/pages/OPC.js
@@ -13,33 +13,35 @@ class Form extends React.Component {
 		this.handlePPOChange = this.handlePPOChange.bind(this);
 		this.handleContractChange = this.handleContractChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
+		this.handleSymbolChange = this.handleSymbolChange.bind(this);
+		this.handleStockPriceChange = this.handleStockPriceChange.bind(this);
+		this.handleLookUp = this.handleLookUp.bind(this);
 	}
 	
-	getTotal() {
+	getTotal(contracts, ppo) {
 		var total;
-		if (this.state.contracts != "" && this.state.ppo != "") {
-			total = parseInt(this.state.contracts) * parseInt(this.state.ppo) * 100;
+		
+		if (contracts !== "" && ppo !== "") {
+			total = parseInt(contracts) * parseInt(ppo) * 100;
 		} else {
 			total = 0;
 		}
 		
-		console.log("Total is: " + total);
-		
 		this.setState({totalCost: total});
 		
+		return total;
 	}
 
 	handlePPOChange(event) {
-		this.setState({ppo: event.target.value});
-		//console.log(this.state.ppo);
-		//this.getTotal(); 
+		var value = event.target.value;
+		this.setState({ppo: value});
+		this.getTotal(this.state.contracts, value); 
 	}
 	
 	handleContractChange(event) {
-		this.setState({contracts: event.target.value});
-		//console.log("Set to: " + event.target.value);
-		//console.log("Contract is: " + this.state.contracts);
-		//this.getTotal();
+		var value = event.target.value;
+		this.setState({contracts: value});
+		this.getTotal(value, this.state.ppo);
 	}
 	
 	handleSymbolChange(event) {
@@ -65,12 +67,19 @@ class Form extends React.Component {
 		return (
 			<form>
 				<div>
-					<h3> Stock Symbol </h3> <br/>
-					<label>
-						Symbol:
-						<input type="text" name="Symbol" onChange={this.handleSymbolChange}/>
-					</label>
-					<input type="button" value="Look Up" onClick={this.handleLookUp}/> 
+					<div>
+						<h3> Stock Symbol </h3>
+					</div>
+					
+					<br/>
+					
+					<span>
+						<label>
+							Symbol:
+							<input type="text" name="Symbol" onChange={this.handleSymbolChange}/>
+						</label>
+						<input type="button" value="Look Up" onClick={this.handleLookUp}/> 
+					</span>
 					
 					<br/>
 					
@@ -84,7 +93,12 @@ class Form extends React.Component {
 				</div>
 				
 				<div>
-					<h3> Option </h3> <br/>
+					<div>
+						<h3> Option </h3> 
+					</div>
+					
+					<br/>
+					
 					<label>
 						Buy or write:
 						<input type="radio" id="buy" name="Buy or Write" value="buy" />
@@ -100,7 +114,7 @@ class Form extends React.Component {
 						Price per Option:
 						<span>
 							$
-							<input type="text" name="PPO" id="PPO" onChange={e => {this.handlePPOChange(e); this.getTotal(e); }} />
+							<input type="text" name="PPO" id="PPO" onChange={this.handlePPOChange} />
 						</span>
 					</label>
 					
@@ -109,7 +123,7 @@ class Form extends React.Component {
 					<label>
 						Contracts:
 						<span>
-							<input type="text" name="Contracts" id="Contracts" onChange={e => {this.handleContractChange(e); this.getTotal(e);}} />
+							<input type="text" name="Contracts" id="Contracts" onChange={this.handleContractChange} />
 							x 100
 						</span>
 					</label>
