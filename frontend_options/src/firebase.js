298b443c1eb52ac946c0e370d37eb732f29e7345
Victor Rinaldi <victorinaldi@ucla.edu>
2021-06-03 11:47:35 -0700

    add firebase authentification + update git ignore

--- frontend_options/src/firebase.js
+++ frontend_options/src/firebase.js
@@ -3,14 +3,15 @@ import firebase from "firebase/app"
 import "firebase/auth"
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: "AIzaSyCMdjEwurne0SvBdLzsN4MUnOFuDwah-vo",
+    authDomain: "auth-development-1e056.firebaseapp.com",
+    databaseURL: "https://auth-development-1e056-default-rtdb.firebaseio.com/",
+    projectId: "auth-development-1e056",
+    storageBucket: "auth-development-1e056.appspot.com",
+    messagingSenderId: "729954982605",
+    appId: "1:729954982605:web:d523efc16e31ce0164e784"
 })
+
 
 export const auth = app.auth()
 export default app
