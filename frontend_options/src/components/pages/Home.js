b7b72b4d3e24c818658971c1155f5c6b70ccd5c3
hoodjustin <justinnerhood@yahoo.com>
2021-06-02 20:54:32 -0700

    updated the home page
    -stabilized spinning b
    -added more info to home page

--- frontend_options/src/components/pages/Home.js
+++ frontend_options/src/components/pages/Home.js
@@ -10,30 +10,20 @@ function Home () {
         <div className="container1">
         <img src="/static/bearfinal-removebg-preview.png" alt="bear" id="bearimage"></img>
         <img src="/static/bull1-removebg-preview.png" alt="bull" id="bullimage"></img>
-        <h1 className="title1">This is the Home Page</h1>
+        <h1 className="title1">we tell you the best times to invest</h1>
             <p className="p1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Architecto, illum qui? Repudiandae sed aliquid ab qui fuga 
-            numquam, reprehenderit voluptate aspernatur alias nesciunt, 
-            s quia fugiat. Laboriosam tempora similique laudantium?
+            With our algorithms, we can detect the ideal times for users 
+            to buy options or to short them. We can do this all using the magic of 
+            bullish and bearish divergence
             </p>
         </div>
         <div className="container2">
-        <h1 className="title1">This is the Home Page</h1>
+        <h1 className="title1">bullish and bearish what?</h1>
             <p className="p1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Architecto, illum qui? Repudiandae sed aliquid ab qui fuga 
-            numquam, reprehenderit voluptate aspernatur alias nesciunt, 
-            s quia fugiat. Laboriosam tempora similique laudantium?
-            </p>
-        </div>
-        <div className="container3">
-        <h1 className="title1">This is the Home Page</h1>
-            <p className="p1">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-            Architecto, illum qui? Repudiandae sed aliquid ab qui fuga 
-            numquam, reprehenderit voluptate aspernatur alias nesciunt, 
-            s quia fugiat. Laboriosam tempora similique laudantium?
+            Bullish Divergence: A sudden reversal from a bearish trend (stock price is going downwards).
+            Bearish Divergence: A sudden reversal from a bullish trend (stock price is going upwards).
+            We use the MACD (moving average convergence divergence) indicator to detect for bullish and 
+            bearish divergences.
             </p>
         </div>
     </nav>
@@ -42,3 +32,9 @@ function Home () {
 }
 
 export default Home;
+//watchlist
+//login
+//options profit calculator
+//bullish and bearish divergence
+//how images are sent to frontend/ticker stuff
+//intro and overview
