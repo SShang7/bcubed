30d93362578e44b1558ffddfde903e08830abced
Andrew Zhu <andrewzhu1223@g.ucla.edu>
2021-06-03 16:01:32 -0700

    Switched Function of Calculator
    Now calculates ROI

--- frontend_options/src/components/pages/OPC.js
+++ frontend_options/src/components/pages/OPC.js
@@ -7,19 +7,28 @@ class Form extends React.Component {
 					  contracts: '',
 					  ppo: '',
 					  symbol: '',
-					  stockPrice: '',
 					  buyOrWrite: '',
-					  callOrPut: ''
+					  callOrPut: '',
+					  expirationDate: '',
+					  openCommission: '0',
+					  exitCommission: '0',
+					  exitDate: '',
+					  exitValue: '',
+					  roi: '',
+					  difference: ''
 		};
 
 		this.handlePPOChange = this.handlePPOChange.bind(this);
 		this.handleContractChange = this.handleContractChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleSymbolChange = this.handleSymbolChange.bind(this);
-		this.handleStockPriceChange = this.handleStockPriceChange.bind(this);
-		this.handleLookUp = this.handleLookUp.bind(this);
 		this.handleCallOrPutChange = this.handleCallOrPutChange.bind(this);
 		this.handleBuyOrWriteChange = this.handleBuyOrWriteChange.bind(this);
+		this.handleExpirationDate = this.handleExpirationDate.bind(this);
+		this.handleOpenCommission = this.handleOpenCommission.bind(this);
+		this.handleExitCommission = this.handleExitCommission.bind(this);
+		this.handleExitDate = this.handleExitDate.bind(this);
+		this.handleExitValue = this.handleExitValue.bind(this);
 	}
 	
 	getTotal(contracts, ppo) {
@@ -34,6 +43,39 @@ class Form extends React.Component {
 		this.setState({totalCost: total});
 		
 		return total;
+	}
+	
+	getDifference(openCommission, exitCommission, exitValue) {
+		var diff;
+		
+		var openValue = this.state.totalCost;
+		
+		if (openCommission !== "" && exitCommission !== "" && exitValue !== "" && openValue !== "") {
+			diff = parseFloat(exitValue) - parseFloat(exitCommission) - (parseFloat(openValue) - parseFloat(openCommission));
+		} else {
+			diff = 0;
+		}
+		
+		this.setState({difference: diff});
+		
+		return diff;
+	}
+	
+	getROI(openCommission) {
+		var ROI;
+		var openValue = this.state.totalCost;
+		
+		if (openCommission !== "" && openValue !== "" && this.state.difference !== "") {
+			var openValue = this.state.totalCost;
+			
+			ROI = parseFloat(this.state.difference) / (parseFloat(openValue) - parseFloat(openCommission));
+		} else {
+			ROI = 0;
+		}
+		
+		this.setState({roi: ROI});
+		
+		return ROI;
 	}
 
 	handlePPOChange(event) {
@@ -52,18 +94,42 @@ class Form extends React.Component {
 		this.setState({symbol: event.target.value});
 	}
 	
-	handleStockPriceChange(event) {
-		this.setState({stockPrice: event.target.value});
-	}
-	
 	handleCallOrPutChange(event) {
-		console.log('Changed callOrPut to ' + event.target.value);
 		this.setState({callOrPut: event.target.value});
 	}
 
 	handleBuyOrWriteChange(event) {
-		console.log('Changed buyOrWrite to ' + event.target.value);
 		this.setState({buyOrWrite: event.target.value});
+	}
+	
+	handleExpirationDate(event) {
+		this.setState({expirationDate: event.target.value});
+	}
+	
+	handleOpenCommission(event) {
+		var value = event.target.value;
+		
+		this.setState({openCommission: value});
+		this.getDifference(value, this.state.exitCommission, this.state.exitValue);
+		this.getROI(value);
+	}
+	
+	handleExitCommission(event) {
+		var value = event.target.value;
+		
+		this.setState({exitCommission: value});
+		this.getDifference(this.state.openCommission, value, this.state.exitValue);
+	}
+	
+	handleExitDate(event) {
+		this.setState({exitDate: event.target.value});
+	}
+	
+	handleExitValue(event) {
+		var value = event.target.value;
+		
+		this.setState({exitValue: value});
+		this.getDifference(this.state.openCommission, this.state.exitCommission, value);
 	}
 
 	handleSubmit(event) {
@@ -72,10 +138,6 @@ class Form extends React.Component {
 		event.preventDefault();
 	}
 	
-	handleLookUp(event) {
-		alert('Pretend we looked up ' + this.state.symbol + '\'s stock price.');
-		this.setState({stockPrice: '1234'});
-	}
 	//<!onSubmit={this.handleSubmit}>
 	render() {
 		const highlightColor = '#3500d3'; //#3500d3
@@ -127,17 +189,17 @@ class Form extends React.Component {
 				<div>
 					<div style={divStyle}>
 						<h2 style={h2Style}>
-							What kind of calculator did you want?
+							Input the Requested Info
 						</h2>
 						
 						<br/>
 						
 						<span>
-							<input type="radio" id="long call" name="Call or Put" value="long call" checked={this.state.callOrPut === "long call"} onChange={this.handleCallOrPutChange} style={radioButtonStyle} />
-							<label for="long call" style={radioStyle}> Long Call </label>
+							<input type="radio" id="call" name="Call or Put" value="call" checked={this.state.callOrPut === "call"} onChange={this.handleCallOrPutChange} style={radioButtonStyle} />
+							<label for="long call" style={radioStyle}> Call </label>
 							
-							<input type="radio" id="long put" name="Call or Put" value="long put" checked={this.state.callOrPut === "long put"} onChange={this.handleCallOrPutChange} style={radioButtonStyle} />
-							<label for="long put" style={radioStyle}> Long Put </label>
+							<input type="radio" id="put" name="Call or Put" value="put" checked={this.state.callOrPut === "put"} onChange={this.handleCallOrPutChange} style={radioButtonStyle} />
+							<label for="long put" style={radioStyle}> Put </label>
 						</span>
 					</div>
 				</div>
@@ -151,34 +213,18 @@ class Form extends React.Component {
 					
 					<br/>
 					
-					<div style={divStyle}>
-						<span style={{paddingRight: '20px'}}>
+					<span style={{paddingRight: '20px'}}>
 							Symbol:
-						</span>
+					</span>
 						
-						<input type="text" name="Symbol" onChange={this.handleSymbolChange} style={textboxStyle} />
-						
-						<br/>
-						
-						<input type="button" value="Look Up" onClick={this.handleLookUp} style={button} /> 
-					</div>
-					
-					<br/>
-					
-					<div style={divStyle}>
-						Stock Price:
-						<span style={spanStyle}>
-							$
-							<input type="text" name="Price" value={this.state.stockPrice} onChange={this.handleStockPriceChange} style={textboxStyle} />
-						</span>
-					</div>
+					<input type="text" name="Symbol" onChange={this.handleSymbolChange} style={textboxStyle} />
 				</div>
 				
 				<br/>
 				
 				<div>
 					<div>
-						<h2 style={h2Style}> Option </h2>
+						<h2 style={h2Style}> Open Option </h2>
 					</div>
 					
 					<br/>
@@ -217,16 +263,83 @@ class Form extends React.Component {
 					<br/>
 					
 					<div style={divStyle}>
-						Total Cost:
+						Total Value:
 						<div id="total" value={this.state.totalCost}>{this.state.totalCost}</div>
+					</div>
+					
+					<br/>
+					
+					<div style={divStyle}>
+						Expiration Date:
+						<span style={spanStyle}>
+							<input type="text" name="Expiration Date" id="Expiration Date" onChange={this.handleExpirationDate} style={textboxStyle} placeholder="MM/DD/YYYY" />
+						</span>
+					</div>
+					
+					<br/>
+					
+					<div style={divStyle}>
+						Open Commission:
+						<span style={spanStyle}>
+							<input type="text" name="Open Commission" id="Open Commission" onChange={this.handleOpenCommission} style={textboxStyle} placeholder="0" />
+						</span>
 					</div>
 				</div>
 				
-				<input type="submit" value="Calculate" onClick={this.handleSubmit} style={button} />
+				<br/>
+				
+				<div style={divStyle}>
+				
+					<div style={divStyle}>
+						<h2 style={h2Style}>
+							Close Option
+						</h2>
+					</div>
+					
+					<br/>
+					
+					<div style={divStyle}>
+						Exit Date:
+						<span style={spanStyle}>
+							<input type="text" name="Exit Date" id="Exit Date" onChange={this.handleExitDate} style={textboxStyle} placeholder="MM/DD/YYYY" />
+						</span>
+					</div>
+					
+					<br/>
+					
+					<div style={divStyle}>
+						Exit Value:
+						<span style={spanStyle}>
+							<input type="text" name="Exit Value" id="Exit Value" onChange={this.handleExitValue} style={textboxStyle} />
+						</span>
+					</div>
+					
+					<br/>
+					
+					<div style={divStyle}>
+						Exit Commission:
+						<span style={spanStyle}>
+							<input type="text" name="Exit Commission" id="Exit Commission" onChange={this.handleExitCommission} style={textboxStyle} placeholder="0" />
+						</span>
+					</div>
+					
+					<br/>
+					
+					<div style={divStyle}>
+						Return on Investment:
+						<span style={spanStyle}>
+							<div id="total" value={this.state.roi}>{this.state.roi}</div>
+						</span>
+					</div>
+
+				</div>
+				
 			</form>
 		);
 	}
 }
+
+//<input type="submit" value="Log" onClick={this.handleSubmit} style={button} />
 
 function OPC() {
 	
