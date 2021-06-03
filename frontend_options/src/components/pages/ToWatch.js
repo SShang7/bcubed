ea4b2c2e1b87aadb5e82d2278d6460b17c19ac28
hoodjustin <justinnerhood@yahoo.com>
2021-06-02 18:21:22 -0700

    watchlist
    added watchlist functionality and store favorited  tickers in firebase

--- frontend_options/src/components/pages/ToWatch.js
+++ frontend_options/src/components/pages/ToWatch.js
@@ -1,13 +1,63 @@
 import React from 'react'
+import { useRef } from 'react'
+import firebase from "firebase"
+import { useHistory } from 'react-router-dom'
+
 export default function ToWatch() {
+    const history = useHistory()
+    const tickerRef = useRef()
+    async function sendTicker(e) { 
+        e.preventDefault()
+        var data = {
+            ticker: tickerRef.current.value
+        }
+        await firebase.database().ref('users/' + firebase.auth().currentUser.uid).push(data);
+        history.go(0)
+    }
+
+
     return (
         <>
-            <form>
+            <form onSubmit = {sendTicker}>
                 <div>
                     <label for="exampleInputEmail1">Enter some tickers you'd like to favorite</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ticker"></input>
+                    <input class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ticker" ref={tickerRef}></input>
                 </div>
             </form>
         </>
     )
+    /*const tickerRef = useRef()
+    const { currentUser } = useAuth()
+    async function sendTicker(e) {
+        var firebaseConfig = {
+            apiKey: "AIzaSyCMdjEwurne0SvBdLzsN4MUnOFuDwah-vo",
+            authDomain: "auth-development-1e056.firebaseapp.com",
+            databaseURL: "https://auth-development-1e056-default-rtdb.firebaseio.com",
+            projectId: "auth-development-1e056",
+            storageBucket: "auth-development-1e056.appspot.com",
+            messagingSenderId: "729954982605",
+            appId: "1:729954982605:web:d523efc16e31ce0164e784"
+          };
+          // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        var database = firebase.database();
+        var ref = database.ref('usertickers')
+        e.preventDefault()
+        var data = {
+            email: currentUser.email,
+            ticker: tickerRef.current.value
+        }
+        ref.push(data)
+    }
+
+    return (
+        <>
+            <form onSubmit = {sendTicker}>
+                <div>
+                    <label for="exampleInputEmail1">Enter some tickers you'd like to favorite</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="ticker" ref={tickerRef}></input>
+                </div>
+            </form>
+        </>
+    )*/
 }
--- frontend_options/src/components/pages/Watchlist.js
+++ frontend_options/src/components/pages/Watchlist.js
@@ -1,15 +1,72 @@
-import React from 'react'
-import { useAuth } from '../../contexts/AuthContext'
+import React, { useState } from 'react'
+import firebase from '../../firebase'
+import 'firebase/database'
 import { Link } from 'react-router-dom' 
+import { useAuth } from '../../contexts/AuthContext'
 
-export default function Watchlist() {
+  export default function Watchlist() {
     const { currentUser } = useAuth()
+    
+  class Watchlist extends React.Component {
+    constructor(props) {
+        
+        super(props);
+       
+        this.state = {studentslist : []}
+        }
+        
+      componentDidMount() {
+          firebase.database().ref('users/' + firebase.auth().currentUser.uid).on("value", snapshot => {
+            let studentlist = [];
+            snapshot.forEach(snap => {
+                // snap.val() is the dictionary with all your keys/values from the 'students-list' path
+                studentlist.push(snap.val());
+            });
+            this.setState({ studentslist: studentlist });
+          });
+     }
+      
+      render(){
+      return (
+        <div className="MainDiv">
+          <div class="jumbotron text-center bg-sky">
+              <h3>{currentUser.email}'s tickers</h3>
+          </div>
+        
+          <div className="container">
+              <table id="example" class="display table">
+                <thead class="thead-dark">
+                    <tr>
+                        <th>favorites</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {this.state.studentslist.map(data => {
+                    
+                    return (
+                        <tr>     
+                        <td>{data.ticker}</td>
+                        </tr>
+                        
+                    );
+                   
+                    })}
+            
+                   
+                </tbody>
+                
+             </table>
+              
+         </div>
+        </div>
+      );
+    }
+    }
     if(currentUser) {
         return (
-            <div>
-            <h2 className="watchHeader">{currentUser.email}'s watchlist: </h2>
-            <Link className="new" to="/towatch">add some more tickers?</Link>
-            </div>
+            <Watchlist>
+
+            </Watchlist>
         )
     }
     else {
@@ -20,4 +77,5 @@ export default function Watchlist() {
             </div>
         )
     }
-}
+  }
+
